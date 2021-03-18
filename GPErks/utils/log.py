3c97993ccfc5dad3c3293c862f7a48457682757c
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2021-03-18 18:06:09 +0100

    Logger

---
+++ GPErks/utils/log.py
@@ -0,0 +1,23 @@
+import logging
+
+from GPErks.constants import (
+    APPLICATION_NAME,
+    DEFAULT_LOGGING_FILE,
+    DEFAULT_LOG_FORMAT,
+)
+
+
+def get_logger(
+        name=APPLICATION_NAME,
+        log_format=DEFAULT_LOG_FORMAT,
+        stdout_level=logging.INFO,
+        file_name=DEFAULT_LOGGING_FILE,
+        file_level=logging.INFO,
+):
+    logging.basicConfig(level=stdout_level, format=log_format)
+    logger = logging.getLogger(name)
+    file_handler = logging.FileHandler(file_name)
+    file_handler.setLevel(file_level)
+    file_handler.setFormatter(logging.Formatter(log_format))
+    logger.addHandler(file_handler)
+    return logger
