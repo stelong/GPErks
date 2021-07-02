ba05f46cb595d005435564367b5c84941becdd6e
Stefano Longobardi <sl18@bioeng306-lap.isd.kcl.ac.uk>
2021-07-02 16:43:40 +0200

    tidying up a bit

--- GPErks/constants.py
+++ GPErks/constants.py
@@ -10,3 +10,12 @@ DEFAULT_LOG_FORMAT = (
     "%(levelname)s:%(asctime)s:%(module)s:%(funcName)s:"
     "L%(lineno)d: %(message)s"
 )
+
+# gsa constants:
+N = 1024
+N_DRAWS = 1000
+THRESHOLD = 0.01
+
+# plots constants:
+HEIGHT = 9.36111
+WIDTH = 5.91667
