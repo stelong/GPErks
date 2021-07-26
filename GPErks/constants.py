85af6886ca20d30010e8cd3db83f9c87879aeb63
Stefano Longobardi <sl18@bioeng306-lap.isd.kcl.ac.uk>
2021-07-26 08:58:06 +0200

    added new constants

--- GPErks/constants.py
+++ GPErks/constants.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from scipy.stats import norm
+
 APPLICATION_NAME = "GPErks"
 
 # logging
@@ -12,9 +14,14 @@ DEFAULT_LOG_FORMAT = (
 )
 
 # gsa constants:
+CONF_LEVEL = 0.95
+Z = norm.ppf(
+    0.5 + CONF_LEVEL / 2
+)  # needed for making SALib return the original and NOT the scaled std
 N = 1024
 N_BOOTSTRAP = 100
 N_DRAWS = 1000
+SKIP_VALUES = 0
 THRESHOLD = 0.01
 
 # plots constants:
