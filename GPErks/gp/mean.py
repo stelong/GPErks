690d05a04dd402017a77eccdf3dbdc6cd56ac500
Stefano <stefano.longobardi.8@gmail.com>
2022-12-11 16:27:01 +0000

    New functionality: custom LinearMean module with higher than first degreed polynomials

--- GPErks/gp/mean.py
+++ GPErks/gp/mean.py
@@ -1,19 +1,37 @@
 import gpytorch
+import numpy
 import torch
+
+from GPErks.utils.polynomialfeatures import PolynomialFeatures
 
 
 class LinearMean(gpytorch.means.Mean):
-    def __init__(self, input_size, data_mean, batch_shape=torch.Size()):
+    def __init__(self, degree, input_size, batch_shape=torch.Size(), bias=True):
         super().__init__()
+        self.degree = degree
+        self.input_size = input_size
+
+        poly = PolynomialFeatures(self.degree)
+        poly.fit(self.input_size)
+
+        n_weights = len(poly.indices)
         self.register_parameter(
             name="weights",
-            parameter=torch.nn.Parameter(torch.zeros(*batch_shape, input_size, 1)),
-        )
-        self.register_parameter(
-            name="bias",
-            parameter=torch.nn.Parameter(data_mean * torch.ones(*batch_shape, 1)),
+            parameter=torch.nn.Parameter(torch.randn(*batch_shape, n_weights, 1)),
         )
 
+        if bias:
+            self.register_parameter(
+                name="bias", parameter=torch.nn.Parameter(torch.randn(*batch_shape, 1))
+            )
+        else:
+            self.bias = None
+
     def forward(self, x):
-        res = self.bias + x.matmul(self.weights).squeeze(-1)
+        poly = PolynomialFeatures(self.degree)
+        poly.fit(self.input_size)
+        x_ = poly.transform(x)
+        res = x_.matmul(self.weights).squeeze(-1)
+        if self.bias is not None:
+            res = res + self.bias
         return res
