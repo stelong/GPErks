84bf1c0586151e2b0a58d22990143b96e9b0c938
Stefano <stefano.longobardi.8@gmail.com>
2022-12-12 12:47:12 +0000

    New functionality: can now inspect linear mean module fit both in 1D and 2D

--- GPErks/plot/mean.py
+++ GPErks/plot/mean.py
@@ -3,62 +3,79 @@ import numpy as np
 
 from GPErks.constants import DEFAULT_INFERENCE_GRID_DIM, WIDTH, HEIGHT
 from GPErks.train.emulator import GPEmulator
-from GPErks.utils.array import get_minmax, tensorize
+from GPErks.utils.array import tensorize
 
 
 def inspect_mean_module(
-		emulator: GPEmulator,
-		grid_dim: int = DEFAULT_INFERENCE_GRID_DIM,
-	):
-	if emulator.experiment.mean_module.input_size != 2:
-		raise ValueError(
-			"self.input_size does not equal 2! "
-			+ "You can only inspect the mean module of emulators trained on a 2D parameter space."
-		)
+    emulator: GPEmulator,
+    grid_dim: int = DEFAULT_INFERENCE_GRID_DIM,
+):
+    input_size = emulator.experiment.mean_module.input_size
 
-	X = emulator.scaled_data.X_train
-	y = emulator.scaled_data.y_train
-	xlabels = emulator.experiment.dataset.x_labels
-	ylabel = emulator.experiment.dataset.y_label
-	minmax = get_minmax(X)
+    if input_size > 2:
+        raise ValueError(
+            "self.input_size is greater than 2! "
+            + "You can only inspect the mean module of emulators trained on a 1D/2D parameter space."
+        )
+    else:
+        x = emulator.scaled_data.X_train
+        y = emulator.scaled_data.y_train
 
-	x1 = np.linspace(minmax[0, 0], minmax[0, 1], grid_dim)
-	x2 = np.linspace(minmax[1, 0], minmax[1, 1], grid_dim)
-	x1, x2 = np.meshgrid(x1, x2)
-	X_grid = np.hstack((x1.reshape(-1, 1), x2.reshape(-1, 1)))
-	y_grid = (
-		emulator.experiment.mean_module.forward(tensorize(X_grid))
-		.cpu()
-		.detach()
-		.numpy()
-	)
-	y_grid = y_grid.reshape(grid_dim, grid_dim)
+        test_avail = not (emulator.experiment.dataset.X_test is None)
+        if test_avail:
+            x_test = emulator.scaled_data.scx.transform(
+                emulator.experiment.dataset.X_test
+            )
+            y_test = emulator.scaled_data.scy.transform(
+                emulator.experiment.dataset.y_test
+            )
 
-	fig = plt.figure(figsize=(2 * WIDTH, 2 * HEIGHT / 3))
-	axis = fig.add_subplot(111, projection="3d")
+        xlabels = emulator.experiment.dataset.x_labels
+        ylabel = emulator.experiment.dataset.y_label
+        mean_module = emulator.experiment.mean_module
 
-	axis.scatter3D(X[:, 0], X[:, 1], y, c=y, cmap="Reds", label="training data")
-	if emulator.experiment.dataset.X_test is not None:
-		X_test = emulator.scaled_data.scx.transform(emulator.experiment.dataset.X_test)
-		y_test = emulator.scaled_data.scy.transform(emulator.experiment.dataset.y_test)
-		axis.scatter3D(
-			X_test[:, 0],
-			X_test[:, 1],
-			y_test,
-			c=y_test,
-			cmap="Greens",
-			label="testing data",
-		)
+        if input_size == 1:
+            x_grid = np.linspace(0, 1, grid_dim**2).reshape(-1, 1)
+            y_grid = mean_module.forward(tensorize(x_grid)).cpu().detach().numpy()
 
-	axis.plot_surface(x1, x2, y_grid, rstride=1, cstride=1, color="b", alpha=0.5)
+            # 1D->1D plot
+            fig, axis = plt.subplots(1, 1)
+            axis.plot(x_grid, y_grid, c="C0")
+            axis.scatter(x, y, fc="C3", label="training data")
+            if test_avail:
+                axis.scatter(x_test, y_test, fc="C2", label="testing data")
+            axis.set_xlabel(xlabels[0])
+            axis.set_ylabel(ylabel)
 
-	axis.legend()
-	axis.set_xlabel(xlabels[0])
-	axis.set_ylabel(xlabels[1])
-	axis.set_zlabel(ylabel)
+        else:
+            x1, x2 = np.meshgrid(
+                np.linspace(0, 1, grid_dim), np.linspace(0, 1, grid_dim)
+            )
+            x_grid = np.hstack((x1.reshape(-1, 1), x2.reshape(-1, 1)))
+            y_grid = mean_module.forward(tensorize(x_grid)).cpu().detach().numpy()
+            y_grid = y_grid.reshape(grid_dim, grid_dim)
 
-	plt.title(
-		f"LinearMean(degree={emulator.experiment.mean_module.degree})"
-	)
-	fig.tight_layout()
-	plt.show()
+            # 2D->1D plot
+            fig = plt.figure(figsize=(2 * WIDTH, 2 * HEIGHT / 3))
+            axis = fig.add_subplot(111, projection="3d")
+            axis.plot_surface(
+                x1, x2, y_grid, rstride=1, cstride=1, color="b", alpha=0.5
+            )
+            axis.scatter3D(x[:, 0], x[:, 1], y, c=y, cmap="Reds", label="training data")
+            if test_avail:
+                axis.scatter3D(
+                    x_test[:, 0],
+                    x_test[:, 1],
+                    y_test,
+                    c=y_test,
+                    cmap="Greens",
+                    label="testing data",
+                )
+            axis.set_xlabel(xlabels[0])
+            axis.set_ylabel(xlabels[1])
+            axis.set_zlabel(ylabel)
+
+        axis.legend()
+        plt.title(f"LinearMean(degree={mean_module.degree})")
+        fig.tight_layout()
+        plt.show()
