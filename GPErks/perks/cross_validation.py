ff4303df567ad5969d57a93a3639addd293cdee6
Stefano <stefano.longobardi.8@gmail.com>
2023-01-11 19:10:39 +0000

    K-Fold CV can now print statistics and return best performing emulator for further use

--- GPErks/perks/cross_validation.py
+++ GPErks/perks/cross_validation.py
@@ -1,7 +1,11 @@
+import os
 from copy import deepcopy
 from itertools import cycle
 from typing import List, Optional
 
+import numpy as np
+import pandas as pd
+import torch
 from sklearn.model_selection import KFold
 
 from GPErks.constants import (
@@ -12,6 +16,7 @@ from GPErks.constants import (
     DEFAULT_TRAIN_SNAPSHOT_EPOCH_TEMPLATE,
     DEFAULT_TRAIN_SNAPSHOT_RESTART_TEMPLATE,
     DEFAULT_TRAIN_SNAPSHOT_SPLIT_TEMPLATE,
+    DEFAULT_GSA_N_DRAWS,
 )
 from GPErks.gp.data.dataset import Dataset
 from GPErks.gp.experiment import GPExperiment
@@ -56,6 +61,10 @@ class KFoldCrossValidation(Trainable):
             random_state=random_state,
         )
         self.leftout_is_val = leftout_is_val
+        self.best_split = None
+        self.best_split_idx = [None, None]
+        self.best_test_scores_structured_dct = dict()
+        self.emulator: Optional[GPEmulator] = None
 
     def train(
         self,
@@ -88,36 +97,95 @@ class KFoldCrossValidation(Trainable):
                 y[idx_train],
                 X[idx_leftout],
                 y[idx_leftout],
+                idx_train,
+                idx_leftout,
             )
             for i, (device, (idx_train, idx_leftout)) in enumerate(
                 zip(cycle(self.devices), self.split_generator.split(X))
             )
-        }  # TODO: dump to file used indices to reproduce splitting outside here
+        }
 
         best_model_dct = {}
         best_train_stats_dct = {}
-        inference_scores_dct = {}
+        best_test_scores_dct = {}
+        split_idx_dct = {}
 
         for split, (
             best_model,
             best_train_stats,
-            emulator,
+            best_test_scores,
+            idx,
         ) in execute_task_in_parallel(
             self._train_split, splits, self.max_workers
         ).items():
-            inference = Inference(emulator)
-            inference.summary()
 
             best_model_dct[split] = best_model
             best_train_stats_dct[split] = best_train_stats
-            inference_scores_dct[split] = inference.scores_dct
+            best_test_scores_dct[split] = best_test_scores
+            split_idx_dct[split] = idx
 
-        test_scores = {get_metric_name(m): [] for m in self.experiment.metrics}
-        for split, score in inference_scores_dct.items():
-            for m in test_scores.keys():
-                test_scores[m].append(score[m].item())
+        self.best_test_scores_structured_dct = {
+            get_metric_name(m): [] for m in self.experiment.metrics
+        }
+        for split, score in best_test_scores_dct.items():
+            for m in self.best_test_scores_structured_dct.keys():
+                self.best_test_scores_structured_dct[m].append(score[m].item())
 
-        return best_model_dct, best_train_stats_dct, test_scores
+        self.best_split = (
+            np.argmax(self.best_test_scores_structured_dct["R2Score"])
+            if "R2Score" in self.best_test_scores_structured_dct.keys()
+            else 0
+        )
+        self.best_split_idx = split_idx_dct[self.best_split]
+
+        self.emulator = self._get_emulator()
+
+        return best_model_dct, best_train_stats_dct
+
+    def summary(self):
+        data = np.array([x for x in self.best_test_scores_structured_dct.values()])
+        data = np.hstack(
+            (data, data.mean(axis=1).reshape(-1, 1), data.std(axis=1).reshape(-1, 1))
+        )
+        df = pd.DataFrame(
+            data=np.around(data, decimals=4),
+            index=list(self.best_test_scores_structured_dct.keys()),
+            columns=[f"Split {i}" for i in range(self.n_splits)] + ["Mean", "Std"],
+        )
+        print(df)
+
+    def _get_emulator(self):
+        snapshot_dir = posix_path(
+            DEFAULT_TRAIN_SNAPSHOT_DIR,
+            DEFAULT_TRAIN_SNAPSHOT_SPLIT_TEMPLATE.format(split=self.best_split),
+        )
+        best_model_file = posix_path(snapshot_dir, "best_model.pth")
+        best_model_state = torch.load(best_model_file, map_location=torch.device("cpu"))
+
+        dataset = Dataset(
+            self.experiment.dataset.X_train[self.best_split_idx[0]],
+            self.experiment.dataset.y_train[self.best_split_idx[0]],
+            X_test=self.experiment.dataset.X_train[self.best_split_idx[1]],
+            y_test=self.experiment.dataset.y_train[self.best_split_idx[1]],
+            x_labels=self.experiment.dataset.x_labels,
+            y_label=self.experiment.dataset.y_label,
+        )
+
+        experiment = GPExperiment(
+            dataset,
+            deepcopy(self.experiment.likelihood),
+            deepcopy(self.experiment.mean_module),
+            deepcopy(self.experiment.covar_module),
+            n_restarts=self.experiment.n_restarts,
+            seed=self.experiment.seed,
+            metrics=self.experiment.metrics,
+            learn_noise=self.experiment.learn_noise,
+        )
+
+        emulator = GPEmulator(experiment, "cpu")
+        emulator.model.load_state_dict(best_model_state)
+
+        return emulator
 
     def _train_split(
         self,
@@ -130,6 +198,8 @@ class KFoldCrossValidation(Trainable):
         y_train,
         X_leftout,
         y_leftout,
+        idx_train,
+        idx_leftout,
     ):
         log.info(f"Running K-fold split {i}...")
 
@@ -170,13 +240,11 @@ class KFoldCrossValidation(Trainable):
         optimizer = optimizer.__class__(
             experiment.model.parameters(), **optimizer.defaults
         )
-        early_stopping_criterion = deepcopy(early_stopping_criterion)
-        snapshotting_criterion = deepcopy(snapshotting_criterion)
-        snapshotting_criterion.snapshot_dir = (
-            snapshotting_criterion.snapshot_dir.replace(
-                "{restart}", "{{restart}}"
-            ).format(split=i)
-        )
+        esc = deepcopy(early_stopping_criterion)
+        snpc = deepcopy(snapshotting_criterion)
+        snpc.snapshot_dir = snpc.snapshot_dir.replace(
+            "{restart}", "{{restart}}"
+        ).format(split=i)
 
         emulator = GPEmulator(
             experiment,
@@ -184,9 +252,17 @@ class KFoldCrossValidation(Trainable):
         )
         best_model, best_train_stats = emulator.train(
             optimizer,
-            early_stopping_criterion,
-            snapshotting_criterion,
+            early_stopping_criterion=esc,
+            snapshotting_criterion=snpc,
         )
         log.info(f"Run K-fold split {i}.")
 
-        return best_model, best_train_stats, emulator
+        inference = Inference(emulator)
+        inference.summary(printtoconsole=False)
+
+        return (
+            best_model,
+            best_train_stats,
+            inference.scores_dct,
+            [idx_train, idx_leftout],
+        )
