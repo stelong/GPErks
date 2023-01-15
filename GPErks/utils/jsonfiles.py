b64c28ec5a1ad9741486ec7ff102789d2e6e3c5b
Stefano <stefano.longobardi.8@gmail.com>
2023-01-15 14:56:37 +0000

    File is always a json

--- GPErks/utils/jsonfiles.py
+++ GPErks/utils/jsonfiles.py
@@ -15,7 +15,7 @@ class NumpyEncoder(json.JSONEncoder):
 
 
 def save_json(dct, filename):
-    with open(filename + ".json", "w") as f:
+    with open(filename, "w") as f:
         json.dump(dct, f, cls=NumpyEncoder, indent=4)
     return
 
@@ -30,6 +30,6 @@ def numpy_hook(dct):
 
 def load_json(filename):
     dct = {}
-    with open(filename + ".json", "r") as f:
+    with open(filename, "r") as f:
         dct = json.load(f, object_hook=numpy_hook)
     return dct
