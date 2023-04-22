ca5da98b45a4dbb12c937aee37b0b80ec1bbe43c
Stefano <stefano.longobardi.8@gmail.com>
2023-04-22 19:57:57 +0100

    New functionality added: building Dataset from json file!

--- GPErks/gp/data/dataset.py
+++ GPErks/gp/data/dataset.py
@@ -1,4 +1,6 @@
-from typing import Callable, List, Optional
+import json
+from pathlib import Path
+from typing import Callable, List, Optional, Union
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -7,7 +9,6 @@ from scipy.stats import qmc
 from GPErks.constants import HEIGHT, WIDTH
 from GPErks.plot.options import PlotOptions
 from GPErks.plot.plottable import Plottable
-from GPErks.utils.random import RandomEngine
 from GPErks.utils.sampling import Sampler
 
 
@@ -24,26 +25,37 @@ class Dataset(Plottable):
         y_label: Optional[str] = None,
         l_bounds: Optional[List[float]] = None,
         u_bounds: Optional[List[float]] = None,
+        name: Optional[str] = None,
     ):
         super(Dataset, self).__init__()
         self.X_train: np.ndarray = X_train
         self.y_train: np.ndarray = y_train
         self.X_val: Optional[np.ndarray] = X_val
         self.y_val: Optional[np.ndarray] = y_val
+        self.with_val: bool = X_val is not None and y_val is not None
         self.X_test: Optional[np.ndarray] = X_test
         self.y_test: Optional[np.ndarray] = y_test
-        self.with_val: bool = X_val is not None and y_val is not None
-
+        self.with_test: bool = X_test is not None and y_test is not None
         self.sample_size = self.X_train.shape[0]
         self.input_size = self.X_train.shape[1] if len(self.X_train.shape) > 1 else 1
-
         self.x_labels: List[str] = (
             x_labels if x_labels else [f"X{i+1}" for i in range(self.input_size)]
         )
         self.y_label: str = y_label if y_label else "y"
+        self.l_bounds: List[float] = l_bounds if l_bounds else np.min(X_train, axis=0)
+        self.u_bounds: List[float] = u_bounds if u_bounds else np.max(X_train, axis=0)
+        self.name: str = name if name else "TestExperiment"
 
-        self.l_bounds = l_bounds
-        self.u_bounds = u_bounds
+        msg = f"{self.name} dataset loaded. Discrepancy: {self.discrepancy:.6f}."
+        # TODO: here we should read from the json file whether the author of the dataset provided an 'info' key.
+        # A description of what the dataset is about in the 'info' field, then pass this to a self.descr attribute here.
+        # Then this print(msg) should actually be a function that encapsulate all the description we want to display
+        # about the dataset itself, e.g. size of the dataset, which x/y-labels, if it has Validation available: Yes/No
+        # etc. Keeping it simple for the moment.
+        # TODO: now this msg gets printed multiple times for all the y-labels that we have, fix this
+        # TODO: pass self.name to Experiment, I remember some time ago I want to save something using Experiment name
+        # but I cannot remember now what that is
+        print(msg)
 
     def plot(self, plot_options: PlotOptions = PlotOptions()):
         self.plot_train()
@@ -75,7 +87,9 @@ class Dataset(Plottable):
             axis.set_xlabel(self.x_labels[i], fontsize=12)
         axes[0].set_ylabel(self.y_label, fontsize=12)
 
-        plt.suptitle(f"Sample dimension = {sample_size} points", fontsize=12)
+        plt.suptitle(
+            f"{self.name} - Sample dimension = {sample_size} points", fontsize=12
+        )
         plt.show()
 
     def _plot_pairwise(self, X):
@@ -100,8 +114,83 @@ class Dataset(Plottable):
             if j == self.input_size - 1:
                 axis.set_xlabel(self.x_labels[i])
 
-        plt.suptitle(f"Sample dimension = {sample_size} points", fontsize=12)
+        plt.suptitle(
+            f"{self.name} - Sample dimension = {sample_size} points", fontsize=12
+        )
         plt.show()
+
+    @property
+    def discrepancy(self):
+        """Quantifies the distance between the continuous uniform distribution on a hypercube and the discrete uniform
+        distribution on distinct sample points. The lower the value, the better the coverage of the parameter space
+        """
+        X_train_unit_cube = qmc.scale(
+            self.X_train, self.l_bounds, self.u_bounds, reverse=True
+        )
+        return qmc.discrepancy(X_train_unit_cube)
+
+    @classmethod
+    def build_from_file(
+        cls,
+        path_to_data: Optional[Union[Path, str]],
+    ):
+        if isinstance(path_to_data, str):
+            path_to_data = Path(path_to_data)
+        name = path_to_data.stem
+
+        with open(path_to_data, "r") as f:
+            data = json.load(f)
+
+        if "X_train" not in data.keys() or "Y_train" not in data.keys():
+            raise ValueError("Dataset does not contain a training dataset!")
+        X_train = np.array(data["X_train"])
+        Y_train = np.array(data["Y_train"]).T
+
+        x_labels, y_labels = None, Y_train.shape[1] * [None]
+        if "x_labels" in data.keys():
+            x_labels = data["x_labels"]
+        if "y_labels" in data.keys():
+            y_labels = data["y_labels"]
+
+        l_bounds, u_bounds = None, None
+        if "l_bounds" in data.keys():
+            x_labels = data["l_bounds"]
+        if "u_bounds" in data.keys():
+            y_labels = data["u_bounds"]
+
+        with_val = False
+        X_val, Y_val = None, Y_train.shape[1] * [None]
+        if "X_val" in data.keys() and "Y_val" in data.keys():
+            with_val = True
+            X_val = np.array(data["X_val"])
+            Y_val = np.array(data["Y_val"]).T
+
+        with_test = False
+        X_test, Y_test = None, Y_train.shape[1] * [None]
+        if "X_test" in data.keys() and "Y_test" in data.keys():
+            with_test = True
+            X_test = np.array(data["X_test"])
+            Y_test = np.array(data["Y_test"]).T
+
+        datasets = {
+            y_label: cls(
+                X_train,
+                y_train,
+                X_val=X_val,
+                y_val=y_val,
+                X_test=X_test,
+                y_test=y_test,
+                x_labels=x_labels,
+                y_label=y_label,
+                l_bounds=l_bounds,
+                u_bounds=u_bounds,
+                name=name,
+            )
+            for (y_train, y_val, y_test, y_label) in zip(
+                Y_train, Y_val, Y_test, y_labels
+            )
+        }
+        return datasets
 
     @classmethod
     def build_from_function(
@@ -117,6 +206,7 @@ class Dataset(Plottable):
         y_label: Optional[str] = None,
         l_bounds: Optional[List[float]] = None,
         u_bounds: Optional[List[float]] = None,
+        name: Optional[str] = None,
     ):
         sampler = Sampler(design, d, seed)
 
@@ -146,4 +236,5 @@ class Dataset(Plottable):
             y_label=y_label,
             l_bounds=l_bounds,
             u_bounds=u_bounds,
+            name=name,
         )
