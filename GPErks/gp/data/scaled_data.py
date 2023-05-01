2416711d5598176b9d297aaeec8a00b6ecb57722
Stefano <stefano.longobardi.8@gmail.com>
2023-05-01 19:05:29 +0100

    Little improvements

--- GPErks/gp/data/scaled_data.py
+++ GPErks/gp/data/scaled_data.py
@@ -1,35 +1,30 @@
 from torch import Tensor
 
+from GPErks.gp.data.data_scaler import InputDataScaler, OutputDataScaler
 from GPErks.gp.data.dataset import Dataset
 from GPErks.utils.array import tensorize
 
 
 class ScaledData:
+    """ScaledData class contains..."""
+
     def __init__(
         self,
         dataset: Dataset,
-        X_scaler,
-        y_scaler,
+        scx: InputDataScaler,
+        scy: OutputDataScaler,
     ):
-        # TODO: automatic reshape when X_train has size 1 (one point)
-        self.scx = X_scaler
+        self.scx = scx
         self.scx.fit(dataset.X_train)
         self.X_train: Tensor = tensorize(self.scx.transform(dataset.X_train))
-        self.scy = y_scaler
+        self.scy = scy
         self.scy.fit(dataset.y_train)
         self.y_train: Tensor = tensorize(self.scy.transform(dataset.y_train))
-        self.sample_size = (
-            self.X_train.shape[0]
-            if len(self.X_train.shape) > 1
-            else len(dataset.X_train)
-        )
-        self.input_size = (
-            dataset.input_size
-        )  # do we use ScaledData.input_size somewhere?
-
-        self.X_val = None
-        self.y_val = None
+        self.sample_size = dataset.sample_size
+        self.input_size = dataset.input_size
         self.with_val = dataset.with_val
+        self.X_val = dataset.X_val
+        self.y_val = dataset.y_val
         if self.with_val:
-            self.X_val: Tensor = tensorize(self.scx.transform(dataset.X_val))
-            self.y_val: Tensor = tensorize(self.scy.transform(dataset.y_val))
+            self.X_val: Tensor = tensorize(self.scx.transform(self.X_val))
+            self.y_val: Tensor = tensorize(self.scy.transform(self.y_val))
