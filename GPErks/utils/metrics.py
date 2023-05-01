21fb2bc3857572d97072801f143cec66c0b4fb6f
Stefano <stefano.longobardi.8@gmail.com>
2023-05-01 19:06:41 +0100

    Implemented ISE metric as a torchmetrics-like class

--- GPErks/utils/metrics.py
+++ GPErks/utils/metrics.py
@@ -1,4 +1,6 @@
+import numpy
 import torch
+from torch import Tensor
 from torchmetrics import Metric
 
 
@@ -6,21 +8,15 @@ def get_metric_name(metric: Metric) -> str:
     return metric.__class__.__name__
 
 
-def IndependentStandardError(y_true, y_pred_mean, y_pred_std):
-    ise = torch.abs(y_true - y_pred_mean) / y_pred_std
-    return 100.0 * len(torch.where(ise < 2.0)[0]) / len(ise)
+class IndependentStandardError:
+    def __init__(self, ci: float = 2.0):
+        self.ci: float = ci
+        self.total: float = 0.0
 
-
-def MAPE(y, y_pred):
-    n_samples = y.size()[0]
-    y_c = y.detach().clone()
-    l = torch.where(y == 0)[0].tolist()
-    if l:
-        nl = list(set(range(n_samples)) - set(l))
-        correction = torch.min(torch.abs(y[nl]))
-        for idx in l:
-            y_c[idx] = correction
-
-    abs_rel_errors = torch.abs((y - y_pred)) / torch.abs(y_c)
-    sum_of_abs_rel_errors = torch.sum(abs_rel_errors).item()
-    return 100.0 * sum_of_abs_rel_errors / n_samples
+    def __call__(
+        self, y_pred_mean: Tensor, y_pred_std: Tensor, y_true: Tensor
+    ) -> Tensor:
+        ise = torch.abs(y_true - y_pred_mean) / y_pred_std
+        return torch.from_numpy(
+            numpy.array(len(torch.where(ise < self.ci)[0]) / len(ise))
+        )
