241f1a3bc22cf3e1711e8c0adf6ef9aaeb264d7e
Stefano Longobardi <stefano.longobardi.8@gmail.com>
2023-09-05 09:00:46 +0100

    Fix to multiple times logging + improved logging

--- GPErks/log/logger.py
+++ GPErks/log/logger.py
@@ -12,12 +12,23 @@ def get_logger(
     log_format=DEFAULT_LOG_FORMAT,
     stdout_level=logging.INFO,
     file_name=DEFAULT_LOG_FILE,
-    file_level=logging.INFO,
+    file_level=logging.DEBUG,
 ):
-    logging.basicConfig(level=stdout_level, format=log_format)
     logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(log_format)
+
+    stdout_handler = logging.StreamHandler()
+    stdout_handler.setLevel(stdout_level)
+    stdout_handler.setFormatter(formatter)
+
     file_handler = logging.FileHandler(file_name)
     file_handler.setLevel(file_level)
-    file_handler.setFormatter(logging.Formatter(log_format))
-    logger.addHandler(file_handler)
+    file_handler.setFormatter(formatter)
+
+    if not logger.hasHandlers():
+        logger.addHandler(stdout_handler)
+        logger.addHandler(file_handler)
+
     return logger
